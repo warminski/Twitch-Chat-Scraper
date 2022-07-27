63240e5b368fa0edf4109dd1d73ceac3bd45613d
warminski <35105074+warminski@users.noreply.github.com>
2022-07-27 14:08:01 +0200

    added time delay
    added time delay for new messages

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from datetime import datetime
 from csv import writer
+import time as t
 
 #provide stream name
 stream = "https://www.twitch.tv/xqc"
@@ -23,17 +24,21 @@ with open(f'{file_name}', 'w+', newline='', encoding="utf-8") as file:
     writer_object = writer(file)
     while True:
         try:
-            a = driver.find_elements(By.CLASS_NAME, "chat-line__message")
-            b = driver.find_elements(By.CLASS_NAME, "chat-line__message")
-            for i in b:
-                if i not in a:  # avoid adding copies of the same message
-                    username = i.find_element(By.CLASS_NAME, "chat-author__display-name").text
-                    message_text = i.find_element(By.CLASS_NAME, "text-fragment").text
-                    time = datetime.now()
-                    day_string = time.strftime("%d/%m/%Y")  # format datetime
-                    time_string = time.strftime("%H:%M:%S")  # format datetime
-                    row = [day_string, time_string, username, message_text]
-                    writer_object.writerow(row)
+            if len(driver.find_elements(By.CLASS_NAME, "chat-line__message")) == 0:
+                pass
+            else:
+                old_messages = driver.find_elements(By.CLASS_NAME, "chat-line__message")
+                t.sleep(3)  # wait for new messages
+                new_messages = driver.find_elements(By.CLASS_NAME, "chat-line__message")
+                for i in new_messages:
+                    if i not in old_messages:  # avoid adding copies of the same message
+                        username = i.find_element(By.CLASS_NAME, "chat-author__display-name").text
+                        message_text = i.find_element(By.CLASS_NAME, "text-fragment").text
+                        time = datetime.now()
+                        day_string = time.strftime("%d/%m/%Y")  # format datetime
+                        time_string = time.strftime("%H:%M:%S")  # format datetime
+                        row = [day_string, time_string, username, message_text]
+                        writer_object.writerow(row)
 
         except selenium.common.NoSuchElementException:  # continue working even without messages
             pass
